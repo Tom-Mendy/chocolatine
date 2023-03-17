ab8c29b0fa2a0dcbca233d755841a9b8858703ab
Tom Mendy <tom.mendy@epitech.eu>
2023-03-17 18:07:15 +0100

    [TEST] error coding style annotation

--- src/my_project.c
+++ src/my_project.c
@@ -9,15 +9,31 @@
 
 int my_project(int argc, char **argv)
 {
-    char **str = my_load_file_in_array("Makefile");
-    if (str == NULL)
-        return 84;
+    char **str = my_load_file_in_array("Makefile");                     if (str == NULL)return 84;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     for (int i = 0; str[i] != NULL ; i += 1){
-        printf("%s\n", str[i]);
+        printf("%s\n", str[i])  ;
+        //toto
     }
-    for (int i = 0; str[i] != NULL; i += 1){
-        free(str[i]);
-    }
+    for (int i = 0; str[i] != NULL; i += 1)
+    {
+        free(str[i]);}
     free(str);
     return 0;
 }
